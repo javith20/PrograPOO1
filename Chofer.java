153a6acc27df12f1b854888d7ae1dc90e96ccd81
javith20 <43230203+javith20@users.noreply.github.com>
2018-09-27 08:51:36 -0600

    Update and rename Chofer to Chofer.java

---
+++ Chofer.java
@@ -0,0 +1,10 @@
+public class Chofer{
+  private int cedula;
+  private String nombre;
+  private ArrayList<Licencia> listaLicencias;
+  private String correo;
+  private int telefono;
+  
+  Chofer()
+
+}
